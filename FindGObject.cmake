3d5b10b52ae448524d1becab10b6aa9aa8ccde33
tobozo <tobozo@users.noreply.github.com>
2022-02-09 19:18:16 +0100

    better GObject finder

--- FindGObject.cmake
+++ FindGObject.cmake
@@ -1,3 +1,4 @@
+
 # - Try to find GObject
 # Once done this will define
 #
@@ -6,49 +7,60 @@
 #  GOBJECT_LIBRARIES - the libraries needed to use GObject
 #  GOBJECT_DEFINITIONS - Compiler switches required for using GObject
 
-# Copyright (c) 2011, Raphael Kubo da Costa <kubito@gmail.com>
 # Copyright (c) 2006, Tim Beaulen <tbscope@gmail.com>
+# Copyright (c) 2008 Helio Chissini de Castro, <helio@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-FIND_PACKAGE(PkgConfig)
-PKG_CHECK_MODULES(PC_GOBJECT gobject-2.0)
-SET(GOBJECT_DEFINITIONS ${PC_GOBJECT_CFLAGS_OTHER})
 
-FIND_PATH(GOBJECT_INCLUDE_DIR gobject.h
-   HINTS
-   ${PC_GOBJECT_INCLUDEDIR}
-   ${PC_GOBJECT_INCLUDE_DIRS}
-   PATH_SUFFIXES glib-2.0/gobject/
+IF (GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+   # in cache already
+   SET(GObject_FIND_QUIETLY TRUE)
+ELSE (GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+   SET(GObject_FIND_QUIETLY FALSE)
+ENDIF (GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+
+IF (NOT WIN32)
+   FIND_PACKAGE(PkgConfig REQUIRED)
+   # use pkg-config to get the directories and then use these values
+   # in the FIND_PATH() and FIND_LIBRARY() calls
+   PKG_CHECK_MODULES(PKG_GOBJECT2 REQUIRED gobject-2.0)
+   SET(GOBJECT_DEFINITIONS ${PKG_GOBJECT2_CFLAGS})
+ENDIF (NOT WIN32)
+
+FIND_PATH(GOBJECT_INCLUDE_DIR gobject/gobject.h
+   HINTS ${PKG_GOBJECT2_INCLUDE_DIRS} ${PKG_GOBJECT2_INCLUDEDIR}
+   PATHS /usr/include/glib-2.0/
+   PATH_SUFFIXES glib-2.0
    )
+
 
 FIND_LIBRARY(_GObjectLibs NAMES gobject-2.0
    HINTS
-   ${PC_GOBJECT_LIBDIR}
-   ${PC_GOBJECT_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBDIR}
    )
 FIND_LIBRARY(_GModuleLibs NAMES gmodule-2.0
    HINTS
-   ${PC_GOBJECT_LIBDIR}
-   ${PC_GOBJECT_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBDIR}
    )
 FIND_LIBRARY(_GThreadLibs NAMES gthread-2.0
    HINTS
-   ${PC_GOBJECT_LIBDIR}
-   ${PC_GOBJECT_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBDIR}
    )
 FIND_LIBRARY(_GLibs NAMES glib-2.0
    HINTS
-   ${PC_GOBJECT_LIBDIR}
-   ${PC_GOBJECT_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBRARY_DIRS}
+   ${PKG_GOBJECT2_LIBDIR}
    )
 
-SET( GOBJECT_LIBRARIES ${_GObjectLibs} ${_GModuleLibs} ${_GThreadLibs} ${_GLibs} )
+SET (GOBJECT_LIBRARIES ${_GObjectLibs} ${_GModuleLibs} ${_GThreadLibs} ${_GLibs})
+
+MARK_AS_ADVANCED(GOBJECT_INCLUDE_DIR GOBJECT_LIBRARIES)
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GOBJECT DEFAULT_MSG GOBJECT_LIBRARIES GOBJECT_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GObject DEFAULT_MSG GOBJECT_INCLUDE_DIR GOBJECT_LIBRARIES)
 
-MARK_AS_ADVANCED(GOBJECT_INCLUDE_DIR _GObjectLibs _GModuleLibs _GThreadLibs _GLibs)
-
-include_directories(${GOBJECT_INCLUDE_DIR})
